2f9d0e18641c321a3c8e70ec6bfb4a5f6c30cdd0
holy <holysugar@gmail.com>
2013-02-06 18:45:19 +0900

    update test helper

--- test/uuid_validator_test.rb
+++ test/uuid_validator_test.rb
@@ -9,6 +9,17 @@ describe ActiveModel::Validations::UuidValidator do
     I18n.locale = 'en'
   end
 
+  def must_be_valid(key)
+    TestModel.new(key: key).valid?.must_equal true
+  end
+
+  def must_be_error(key, error = 'is not an uuid')
+    model = TestModel.new(key: key)
+    model.valid?.must_equal false
+    model.errors[:key].must_equal [error]
+  end
+
+
   describe "validates :key, :uuid => true" do
     before do
       TestModel.validates :key, :uuid => true
@@ -16,69 +27,51 @@ describe ActiveModel::Validations::UuidValidator do
 
     describe "passes for wikipedia sample" do
       it "is valid" do
-        key = "550e8400-e29b-41d4-a716-446655440000"
-        model = TestModel.new(key: key)
-        model.valid?.must_equal true
+        must_be_valid "550e8400-e29b-41d4-a716-446655440000"
       end
     end
 
     describe "test for SecureRandom.uuid" do
       it "is valid" do
-        model = TestModel.new(key: SecureRandom.uuid)
-        model.valid?.must_equal true
+        must_be_valid SecureRandom.uuid
       end
     end
 
     describe "test for UUIDTools" do
       it "is valid for md5_create" do
-        key = UUIDTools::UUID.md5_create(UUIDTools::UUID_DNS_NAMESPACE, "www.example.com").to_s
-        model = TestModel.new(key: key)
-        model.valid?.must_equal true
+        must_be_valid UUIDTools::UUID.md5_create(UUIDTools::UUID_DNS_NAMESPACE, "www.example.com").to_s
       end
 
       it "is valid for sha1_create" do
-        key = UUIDTools::UUID.sha1_create(UUIDTools::UUID_DNS_NAMESPACE, "www.example.com").to_s
-        model = TestModel.new(key: key)
-        model.valid?.must_equal true
+        must_be_valid UUIDTools::UUID.sha1_create(UUIDTools::UUID_DNS_NAMESPACE, "www.example.com").to_s
       end
 
       it "is valid for timestamp_create" do
-        key = UUIDTools::UUID.timestamp_create.to_s
-        model = TestModel.new(key: key)
-        model.valid?.must_equal true
+        must_be_valid UUIDTools::UUID.timestamp_create.to_s
       end
 
       it "is valid for random_create" do
-        key = UUIDTools::UUID.random_create.to_s
-        model = TestModel.new(key: key)
-        model.valid?.must_equal true
+        must_be_valid UUIDTools::UUID.random_create.to_s
       end
-    end
-
-    def error_in(model)
-      model.valid?.must_equal false
-      model.errors[:key].must_equal ["is not an uuid"]
     end
 
     describe "when nil" do
       it "is invalid" do
-        error_in TestModel.new(key: nil)
+        must_be_error nil
       end
     end
 
     describe "when empty string" do
       it "is invalid" do
-        error_in TestModel.new(key: '')
+        must_be_error ''
       end
     end
 
     describe "when non number" do
       it "is invalid" do
-        key = "550e8400-e29b-41d4-a716-XXXXXXXXXXXX"
-        error_in TestModel.new(key: key)
+        must_be_error "550e8400-e29b-41d4-a716-XXXXXXXXXXXX"
       end
     end
-
   end
 end
 
